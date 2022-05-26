a3b8924794e0f412d7c17c663fdb34cb12bf8886
Yin Chu <ycrijnaard@gmail.com>
2022-05-26 13:50:49 +0300

    Next.js Setup (6)

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
+
+/*
+  NOTES
+  * This is the main entry point for any page in the application. It wraps all other 
+  * Use it to: add UI components that are available on every page (e.g., a navbar or footer)
+  * Use it to: manage the authentication state on the frontend
+*/
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: "John Doe" });
 }
+
+/*
+  NOTES
+  * Only executes on the server
+  * Will not add to the JS bundle size
+*/
