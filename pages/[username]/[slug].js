eaea18df087dc956f5ba7a10de28fa2db4058349
Yin Chu <ycrijnaard@gmail.com>
2022-05-27 15:46:43 +0300

    ISR Incremental Static Regeneration (22)

--- pages/[username]/[slug].js
+++ pages/[username]/[slug].js
@@ -1,3 +1,44 @@
-export default function PostPage({}) {
+import { firestore, getUserWithUsername, postToJSON } from "../../lib/firebase";
+
+export async function getStaticProps({ params }) {
+  const { username, slug } = params;
+  const userDoc = await getUserWithUsername(username);
+
+  let post = null;
+  let path;
+
+  if (userDoc) {
+    const postRef = userDoc.ref.collection("posts").doc(slug);
+    post = postToJSON(await postRef.get());
+
+    path = postRef.path;
+  }
+
+  return {
+    props: { post, path },
+    revalidate: 5000,
+  };
+}
+
+export async function getStaticPaths() {
+  // Improve my using Admin SDK to select empty docs
+  const snapshot = await firestore.collectionGroup("posts").get();
+
+  const paths = snapshot.docs.map((doc) => {
+    const { slug, username } = doc.data();
+    return { params: { username, slug } };
+  });
+
+  return {
+    paths,
+    fallback: "blocking",
+    // Must be in this format:
+    // paths: [
+    //  { params: { username, slug }}
+    // ],
+  };
+}
+
+export default function Post(props) {
   return <main></main>;
 }
