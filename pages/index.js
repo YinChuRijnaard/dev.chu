0dc2ca49bf21ccde4fe526e0dfbca9cfb57ead2c
Yin Chu <ycrijnaard@gmail.com>
2022-05-27 15:29:59 +0300

    SSR + Paginated Home Page Feed (21)

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,64 @@
+import { useState } from "react";
 import Head from "next/head";
 import Link from "next/link";
+import { firestore, fromMillis, postToJSON } from "../lib/firebase";
 import toast from "react-hot-toast";
+import PostFeed from "../components/PostFeed";
 import Loader from "../components/Loader";
 
-export default function Home() {
-  return <div>{/* <button onClick={() => toast.success("Hello!")}>Toast me</button> */}</div>;
+// Max post to query per page
+const LIMIT = 1;
+
+export async function getServerSideProps(context) {
+  const postsQuery = firestore
+    .collectionGroup("posts")
+    .where("published", "==", true)
+    .orderBy("createdAt", "desc")
+    .limit(LIMIT);
+  // collectionGroup: Grab any subcollection, no matter where it's nested in the tree of documents in Firestore and has a name of 'Posts'
+
+  const posts = (await postsQuery.get()).docs.map(postToJSON);
+
+  return { props: { posts } }; // Will be passed to the page component as props
+}
+
+export default function Home(props) {
+  const [posts, setPosts] = useState(props.posts);
+  const [loading, setLoading] = useState(false);
+  const [postsEnd, setPostsEnd] = useState(false);
+
+  const getMorePosts = async () => {
+    setLoading(true);
+    const last = posts[posts.length - 1];
+
+    const cursor = typeof last.createdAt === "number" ? fromMillis(last.createdAt) : last.createdAt;
+
+    const query = firestore
+      .collectionGroup("posts")
+      .where("published", "==", true)
+      .orderBy("createdAt", "desc")
+      .startAfter(cursor)
+      .limit(LIMIT);
+
+    const newPosts = (await query.get()).docs.map((doc) => doc.data());
+
+    setPosts(posts.concat(newPosts));
+    setLoading(false);
+
+    if (newPosts.length < LIMIT) {
+      setPostsEnd(true);
+    }
+  };
+
+  return (
+    <main>
+      <PostFeed posts={posts} admin={undefined} />
+
+      {!loading && !postsEnd && <button onClick={getMorePosts}>Load more</button>}
+
+      <Loader show={loading} />
+
+      {postsEnd && "You have reached the end"}
+    </main>
+  );
 }
