fc0baba48c8be7919704c55bfc2b24246b6eea76
Yin Chu <ycrijnaard@gmail.com>
2022-05-27 19:28:40 +0300

    Post Editing Form (28) - path.split error

--- pages/404.js
+++ pages/404.js
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 export default function Custom404() {
   return (
-    <main>
-      <h1>404 - That page does not seem to exist...</h1>
+    <main className="mx-32 mt-8 flex flex-col items-center space-y-4">
+      <h1 className="text-4xl font-bold">404 - That page does not seem to exist...</h1>
       <iframe
         src="https://giphy.com/embed/l2JehQ2GitHGdVG9y"
         width="480"
@@ -11,7 +11,9 @@ export default function Custom404() {
         frameBorder="0"
         allowFullScreen></iframe>
       <Link href="/">
-        <button className="btn-blue">Go home</button>
+        <button className="flex items-center justify-around rounded-md bg-neutral-900 py-2 px-4 font-bold text-white hover:bg-neutral-800">
+          Go home
+        </button>
       </Link>
     </main>
   );
--- pages/admin/[slug].js
+++ pages/admin/[slug].js
@@ -1,9 +1,107 @@
-import Metatags from "../../components/Metatags";
+import { useState } from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import { useDocumentData } from "react-firebase-hooks/firestore";
+import { useForm } from "react-hook-form";
+import ReactMarkdown from "react-markdown";
+import toast from "react-hot-toast";
+// @ts-ignore
+import styles from "../../styles/Admin.module.css";
+import { auth, firestore, serverTimestamp } from "../../lib/firebase";
+import AuthCheck from "../../components/AuthCheck";
 
-export default function AdminPostEdit({}) {
+export default function AdminPostEdit(props) {
   return (
-    <main>
-      <Metatags title="admin page" description="admin page to edit posts" image={undefined} />
-    </main>
+    <AuthCheck>
+      <PostManager />
+      <h1>Is this working?</h1>
+    </AuthCheck>
+  );
+}
+
+function PostManager() {
+  const [preview, setPreview] = useState(false);
+
+  const router = useRouter();
+  const { slug } = router.query;
+
+  // @ts-ignore
+  const postRef = firestore.collection("users").doc(auth.currentUser.uid).collection("posts").doc(slug);
+  // @ts-ignore
+  const [post] = useDocumentData(postRef);
+
+  return (
+    <>
+      <main className="bg-red-500">
+        {post && (
+          <>
+            <section>
+              <h1>{post.title}</h1>
+              <p>ID: {post.slug}</p>
+
+              <PostForm postRef={postRef} defaultValues={post} preview={preview} />
+            </section>
+
+            <aside>
+              <h3>Tools</h3>
+              <button className="" onClick={() => setPreview(!preview)}>
+                {preview ? "Edit" : "Preview"}
+              </button>
+              <Link href={`/${post.username}/${post.slug}`}>
+                <button className="">Live view</button>
+              </Link>
+            </aside>
+          </>
+        )}
+      </main>
+    </>
+  );
+}
+
+function PostForm({ defaultValues, postRef, preview }) {
+  const { register, handleSubmit, reset, watch } = useForm({ defaultValues, mode: "onChange" });
+
+  const updatePost = async ({ content, published }) => {
+    await postRef.update({
+      content,
+      published,
+      updatedAt: serverTimestamp(),
+    });
+
+    reset({ content, published });
+
+    toast.success("Post updated successfully");
+  };
+
+  return (
+    <form onSubmit={handleSubmit(updatePost)}>
+      {preview && (
+        <div>
+          <ReactMarkdown>{watch("content")}</ReactMarkdown>
+        </div>
+      )}
+
+      {/* className={preview ? styles.hidden : styles.controls} */}
+      <div className={preview ? styles.hidden : styles.controls}>
+        <textarea
+          name="content"
+          // @ts-ignore
+          ref={register}></textarea>
+
+        <fieldset>
+          <input
+            name="published"
+            type="checkbox"
+            // @ts-ignore
+            ref={register}
+          />
+          <label>Published</label>
+        </fieldset>
+
+        <button className="" type="submit">
+          Save changes
+        </button>
+      </div>
+    </form>
   );
 }
